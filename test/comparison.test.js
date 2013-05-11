811c12ef06ef6d9c37998f10092292d3113c640d
jkroso <jkroso@gmail.com>
2013-05-11 13:14:11 +1200

    add simple test for the comparison class

---
+++ test/comparison.test.js
@@ -0,0 +1,17 @@
+
+var exec = require('child_process').exec
+  , should = require('chai').should()
+  , path = require('path')
+  , Comparison = require('../lib/comparison')
+
+describe('comparison', function () {
+	it('should work with files', function (done) {
+		var example = path.resolve(__dirname, '../examples/compare/index.js')
+		exec('node '+example, function(e, out, err){
+			if (e) return done(e)
+			err.should.equal('')
+			out.should.include('nextTick')
+			done()
+		})
+	})
+})
